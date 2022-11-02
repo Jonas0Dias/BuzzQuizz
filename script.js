cba945877a625cdaac5164efd2ec907fbd94f791
Jonas0Dias <jonaaasdiaaas@gmail.com>
2022-11-01 23:02:03 -0300

    Adicionado os quizzes ja existem (Falta formatação)

---
+++ script.js
@@ -0,0 +1,11 @@
+const ObterQuizzes = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes')
+TodosOsQuizzes = document.querySelector('section')
+ObterQuizzes.then(InserirQuizzes)
+
+function InserirQuizzes(resposta){
+    for(i=0;i<resposta.data.length;i++){
+    TodosOsQuizzes.innerHTML = TodosOsQuizzes.innerHTML + `<div class='quiz'><img src=${resposta.data[i].image}></img><p>${resposta.data[i].title}</p></div>`
+    // TodosOsQuizzes.innerHTML='<h1>Todos os Quizzes</h1>'
+    }
+    console.log(resposta.data)
+}
